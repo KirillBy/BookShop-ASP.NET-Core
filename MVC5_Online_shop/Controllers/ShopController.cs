988a5bf77338878a133483a999a71e425432f06c
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-18 15:23:42 +0300

    product list and product card realized method and views and add fancybox

--- MVC5_Online_shop/Controllers/ShopController.cs
+++ MVC5_Online_shop/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using MVC5_Online_shop.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,6 +31,84 @@ namespace MVC5_Online_shop.Controllers
 
             //return partial view
             return PartialView("_CategoryMenuPartial", categoryVMList);
+        }
+
+        // GET: Shop/Category/name
+        public ActionResult Category(string name)
+        {
+            //declare list 
+            List<ProductVM> productVMList;
+
+            using(Db db = new Db())
+            {
+                //get id category
+                CategoryDTO categoryDTO = db.Categories
+                    .Where(x => x.Slug == name).FirstOrDefault();
+
+                int catId = categoryDTO.Id;
+
+                //init list with data
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId)
+                    .Select(x => new ProductVM(x))
+                    .ToList();
+
+                //get category name
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+
+                //null check
+                if(productCat == null)
+                {
+                    var catName = db.Categories.Where(x => x.Slug == name).Select(x => x.Name)
+                        .FirstOrDefault();
+                    ViewBag.CategoryName = catName;
+                }
+                else
+                {
+                    ViewBag.CategoryName = productCat.CategoryName;
+                }
+
+            }
+
+            //return view with model
+            return View(productVMList);
+
+        }
+
+        // GET: Shop/products-details/name
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+            //declare dto and vm models
+            ProductDTO dto;
+            ProductVM model;
+
+            //init ID of product
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+                //check if product is available
+                if(! db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index", "Shop");
+                }
+
+                //init model dto with data
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                //get ID
+                id = dto.Id;
+
+                //init model VM with data
+                model = new ProductVM(dto);
+            }
+
+            //get image from gallery
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id +
+                "/Gallery/Thumbs")).Select(fn => Path.GetFileName(fn));
+
+            //return model to view
+            return View("ProductDetails", model);
         }
     }
 }
