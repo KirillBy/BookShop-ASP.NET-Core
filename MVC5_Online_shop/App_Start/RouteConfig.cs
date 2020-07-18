e974570d0b08e863df6a7c04178126ecd8a2aa9e
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-18 12:58:41 +0300

    routes changed, service pages add to nav bar

--- MVC5_Online_shop/App_Start/RouteConfig.cs
+++ MVC5_Online_shop/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace MVC5_Online_shop
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" },
+                new[] { "MVC5_Online_shop.Controllers" });
+
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" },
+                    new[] { "MVC5_Online_shop.Controllers" });
+
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" },
+                     new[] { "MVC5_Online_shop.Controllers" });
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ MVC5_Online_shop/Controllers/PagesController.cs
@@ -0,0 +1,76 @@
+﻿using MVC5_Online_shop.Models.Data;
+using MVC5_Online_shop.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_Online_shop.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            //get/set slug header
+            if (page == "")
+                page = "home";
+
+
+            //declare model and dto
+            PageVM model;
+            PagesDTO dto;
+
+            //check if page is available
+            using(Db db = new Db())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                    return RedirectToAction("Index", new { page = "" });
+            }
+
+            //get context dto of page
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            //set title of page
+            ViewBag.PageTitle = dto.Title;
+
+            //check if sidebar is exist
+            if(dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            //fill model with data
+            model = new PageVM(dto);
+
+            //return view with model
+            return View(model);
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            //init list PageVM
+            List<PageVM> pageVMList;
+
+            //get all pages except HOME
+            using( Db db = new Db())
+            {
+                pageVMList = db.Pages.ToArray().OrderBy(x => x.Sorting)
+                    .Where(x => x.Slug != "home")
+                    .Select(x => new PageVM(x)).ToList();
+
+            }
+
+            //return partial view and list with data
+            return PartialView("_PagesMenuPartial", pageVMList);
+        }
+    }
+}
