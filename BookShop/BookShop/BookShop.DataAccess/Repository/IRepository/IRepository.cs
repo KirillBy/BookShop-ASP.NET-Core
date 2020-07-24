8a152829ed3a982ae9d9209d0895b5c89713a8f4
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-24 19:52:41 +0300

    implement IRepository inside Repository class

---
+++ BookShop/BookShop/BookShop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace BookShop.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T: class
+    {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filtel = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filtel = null,
+            string includeProperties = null
+             );
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
