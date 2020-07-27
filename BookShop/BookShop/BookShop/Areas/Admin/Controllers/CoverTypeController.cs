c056febf7c88c5a11594467c1b0224dd709f2c4a
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-28 00:03:25 +0300

    cover type models, repos, controller and views add

---
+++ BookShop/BookShop/BookShop/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            CoverType CoverType = new CoverType();
+            if (id == null)
+            {
+                //this is for create
+                return View(CoverType);
+            }
+            //this is for edit
+            CoverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            if (CoverType == null)
+            {
+                return NotFound();              
+            }
+            return View(CoverType);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType CoverType)
+        {
+            if (ModelState.IsValid)
+            {
+                if (CoverType.Id == 0)
+                {
+                    _unitOfWork.CoverType.Add(CoverType);
+
+                }
+                else
+                {
+                    _unitOfWork.CoverType.Update(CoverType);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(CoverType);
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.CoverType.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.CoverType.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+
+        }
+
+        #endregion
+    }
+}
