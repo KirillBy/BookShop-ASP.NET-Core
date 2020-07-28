b92b7947478b8441f36f6fa54e7da2ffeb144fe6
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-28 14:33:04 +0300

    user management block/unblock done

---
+++ BookShop/BookShop/BookShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.DataAccess.Data;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork, ApplicationDbContext db)
+        {
+            _unitOfWork = unitOfWork;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _unitOfWork.ApplicationUser.GetAll(includeProperties: "Company").ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            return Json(new { data = userList });
+        }
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDb = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //user is currently locked, we will unlock them
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Operation Successful." });
+
+        }
+
+        #endregion
+    }
+}
