40a267d00718f1a6c99df978db157cb3989ec169
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-28 08:48:39 +0300

    store procedure for cover type done

--- BookShop/BookShop/BookShop/Areas/Admin/Controllers/CoverTypeController.cs
+++ BookShop/BookShop/BookShop/Areas/Admin/Controllers/CoverTypeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using BookShop.DataAccess.Repository.IRepository;
 using BookShop.Models;
+using BookShop.Utility;
+using Dapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookShop.Areas.Admin.Controllers
@@ -22,44 +24,49 @@ namespace BookShop.Areas.Admin.Controllers
         {
             return View();
         }
-
         public IActionResult Upsert(int? id)
         {
-            CoverType CoverType = new CoverType();
+            CoverType coverType = new CoverType();
             if (id == null)
             {
                 //this is for create
-                return View(CoverType);
+                return View(coverType);
             }
             //this is for edit
-            CoverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
-            if (CoverType == null)
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            coverType = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
+            if (coverType == null)
             {
-                return NotFound();              
+                return NotFound();
             }
-            return View(CoverType);
+            return View(coverType);
 
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(CoverType CoverType)
+        public IActionResult Upsert(CoverType coverType)
         {
             if (ModelState.IsValid)
             {
-                if (CoverType.Id == 0)
+                var parameter = new DynamicParameters();
+                parameter.Add("@Name", coverType.Name);
+
+                if (coverType.Id == 0)
                 {
-                    _unitOfWork.CoverType.Add(CoverType);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Create, parameter);
 
                 }
                 else
                 {
-                    _unitOfWork.CoverType.Update(CoverType);
+                    parameter.Add("@Id", coverType.Id);
+                    _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Update, parameter);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index));
             }
-            return View(CoverType);
+            return View(coverType);
         }
 
 
@@ -68,22 +75,23 @@ namespace BookShop.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.CoverType.GetAll();
+            var allObj = _unitOfWork.SP_Call.List<CoverType>(SD.Proc_CoverType_GetAll, null);
             return Json(new { data = allObj });
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.CoverType.Get(id);
+            var parameter = new DynamicParameters();
+            parameter.Add("@Id", id);
+            var objFromDb = _unitOfWork.SP_Call.OneRecord<CoverType>(SD.Proc_CoverType_Get, parameter);
             if (objFromDb == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-            _unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.SP_Call.Execute(SD.Proc_CoverType_Delete, parameter);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete Successful" });
-
         }
 
         #endregion
