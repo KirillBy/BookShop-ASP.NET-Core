d9d15d8d92a6b156e4634eda26d8525fa805f290
Kiryl Balanovich <balanovichks@gmail.com>
2020-07-28 11:54:24 +0300

    Company model, repo, crud operation and views add, application user model and repo done

---
+++ BookShop/BookShop/BookShop.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,26 @@
+﻿using BookShop.DataAccess.Data;
+using BookShop.DataAccess.Repository.IRepository;
+using BookShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BookShop.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Company company)
+        {
+            _db.Update(company);
+        }
+    }
+}
+
